89293bebcdde9eeb251f3aa26755e0bc08f0ca32
Martin Lau <martinlau@livemail.tw>
2023-06-19 13:11:45 +0800

    update SPM to 5.6

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,7 +9,8 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwifCron",
-            targets: ["SwifCron"]),
+            targets: ["SwifCron"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,9 +21,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwifCron",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "SwifCronTests",
-            dependencies: ["SwifCron"]),
+            dependencies: ["SwifCron"]
+        ),
     ]
 )
